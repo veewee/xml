cff0804a380fc77dbf57036445e6b75786edd61e
Toon Verwerft <toonverwerft@gmail.com>
2021-01-14 20:58:34 +0100

    Dont mute warnings

--- src/Xml/Dom/Manipulator/import_node_deeply.php
+++ src/Xml/Dom/Manipulator/import_node_deeply.php
@@ -7,13 +7,20 @@ namespace VeeWee\Xml\Dom\Manipulator;
 use DOMDocument;
 use DOMNode;
 use Webmozart\Assert\Assert;
+use function Psl\Fun\identity;
+use function Psl\Fun\rethrow;
+use function VeeWee\Xml\ErrorHandling\detect_warnings;
 
 function import_node_deeply(DOMNode $source, DOMNode $target): DOMNode
 {
     $document = $target instanceof DOMDocument ? $target : $target->ownerDocument;
     Assert::notNull($document);
 
-    $result = @$document->importNode($source, true);
+    $result = detect_warnings(fn() => $document->importNode($source, true))->proceed(
+        identity(),
+        rethrow()
+    );
+
     Assert::notFalse($result, 'Cannot import node: Node Type Not Supported');
 
     return $result;
