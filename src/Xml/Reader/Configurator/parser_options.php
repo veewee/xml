878543738be10ae812e99178cf172a471b36d580
Toon Verwerft <toonverwerft@gmail.com>
2021-02-01 16:03:39 +0100

    Trust psalm types

--- src/Xml/Reader/Configurator/parser_options.php
+++ src/Xml/Reader/Configurator/parser_options.php
@@ -14,16 +14,16 @@ use function VeeWee\Xml\ErrorHandling\disallow_issues;
 use function VeeWee\Xml\ErrorHandling\disallow_libxml_false_returns;
 
 /**
- * XMLReader::LOADDTD
+ * \XMLReader::LOADDTD
  * Load DTD but do not validate
  *
- * XMLReader::DEFAULTATTRS
+ * \XMLReader::DEFAULTATTRS
  * Load DTD and default attributes but do not validate
  *
- * XMLReader::VALIDATE
+ * \XMLReader::VALIDATE
  * Load DTD and validate while parsing
  *
- * XMLReader::SUBST_ENTITIES
+ * \XMLReader::SUBST_ENTITIES
  * Substitute entities and expand references
  *
  * @param array<
@@ -39,30 +39,9 @@ use function VeeWee\Xml\ErrorHandling\disallow_libxml_false_returns;
  */
 function parser_options(array $options): callable
 {
-    $allowedOptions = [
-        'LOADDTD' => XMLReader::LOADDTD,
-        'VALIDATE' => XMLReader::VALIDATE,
-        'DEFAULTATTRS' => XMLReader::DEFAULTATTRS,
-        'SUBST_ENTITIES' => XMLReader::SUBST_ENTITIES,
-    ];
-
     return static fn (XMLReader $reader): XMLReader => disallow_issues(
-        static function () use ($reader, $options, $allowedOptions): XMLReader {
+        static function () use ($reader, $options): XMLReader {
             foreach ($options as $property => $value) {
-                Assert::inArray(
-                    $property,
-                    $allowedOptions,
-                    'Could not apply property "'.((string) $property).'", Expected one of:'.join(
-                        values(
-                            map(
-                                keys($allowedOptions),
-                                static fn (string $key): string => 'XMLReader::'.$key
-                            )
-                        ),
-                        ', '
-                    )
-                );
-
                 disallow_libxml_false_returns(
                     $reader->setParserProperty($property, $value),
                     'Unable to set the parser property "'.((string) $property).'" to the XML Reader.'
