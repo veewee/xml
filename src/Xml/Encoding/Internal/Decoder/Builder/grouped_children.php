be818b9985d3ee49fa1d5b28ece03336ef863614
Toon Verwerft <toonverwerft@gmail.com>
2022-02-14 08:34:45 +0100

    Improve handling false values whilst encoding/decoding

--- src/Xml/Encoding/Internal/Decoder/Builder/grouped_children.php
+++ src/Xml/Encoding/Internal/Decoder/Builder/grouped_children.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace VeeWee\Xml\Encoding\Internal\Decoder\Builder;
 
 use DOMElement;
-use function Psl\Dict\filter;
 use function Psl\Dict\map;
 use function Psl\Dict\merge;
 use function Psl\Iter\reduce_with_keys;
@@ -16,25 +15,23 @@ use function Psl\Iter\reduce_with_keys;
  */
 function grouped_children(DOMElement $element): array
 {
-    return filter(
-        reduce_with_keys(
-            group_child_elements($element),
-            /**
-             * @param array $children
-             * @param DOMElement|list<DOMElement> $child
-             * @return array
-             */
-            static fn (array $children, string $name, DOMElement|array $child): array
-                => merge(
-                    $children,
-                    [
-                        $name => is_array($child)
-                            ? [...map($child, static fn (DOMElement $child): array|string
-                                => unwrap_element(element($child)))]
-                            : unwrap_element(element($child))
-                    ]
-                ),
-            [],
-        )
+    return reduce_with_keys(
+        group_child_elements($element),
+        /**
+         * @param array $children
+         * @param DOMElement|list<DOMElement> $child
+         * @return array
+         */
+        static fn (array $children, string $name, DOMElement|array $child): array
+            => merge(
+                $children,
+                [
+                    $name => is_array($child)
+                        ? [...map($child, static fn (DOMElement $child): array|string
+                            => unwrap_element(element($child)))]
+                        : unwrap_element(element($child))
+                ]
+            ),
+        [],
     );
 }
