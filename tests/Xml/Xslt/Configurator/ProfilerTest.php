f2ad360730825da9fcf095427536915bbf011843
Toon Verwerft <toonverwerft@gmail.com>
2025-10-24 14:50:58 +0200

    Fix reformated exception messages

--- tests/Xml/Xslt/Configurator/ProfilerTest.php
+++ tests/Xml/Xslt/Configurator/ProfilerTest.php
@@ -35,7 +35,7 @@ final class ProfilerTest extends TestCase
     public function test_setting_an_invalid_profiler_location_doesnt_result(): void
     {
         $this->expectException(RuntimeException::class);
-        $this->expectExceptionMessage('The file "/THISFOLDERSHOULDNOTEXIST" does not exist.');
+        $this->expectExceptionMessage('"/THISFOLDERSHOULDNOTEXIST"');
 
         Processor::fromTemplateDocument(
             $this->createTemplate(),
